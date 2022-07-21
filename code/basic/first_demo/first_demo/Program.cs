905ef02ec7037feb7c429bc14f8cdf3b5e20433c
lmk <liuminkai2021@163.com>
2022-07-21 20:56:30 +0800

    first commit

---
+++ code/basic/first_demo/first_demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; // using 引用命名空间
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_demo // namespase 命名空间名或项目名
+{
+    internal class Program // class 类名
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
