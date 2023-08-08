065bff6f19acc364ebfa2767bb0bb4306d329da1
lmk <liuminkai2021@163.com>
2023-08-08 23:11:41 +0800

    dotnet-basic first day

---
+++ code/dotnet-basic/20230808-01-hellowword/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230808_01_hellowword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world"); // 控制台打印
+            Console.ReadKey(); // 暂停当前程序，等待按下键盘退出
+        }
+    }
+}
